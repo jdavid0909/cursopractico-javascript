7708f5a5d677b84e74894f57e0371b7e6ff6a20d
Josue Rodriguez <jdavid108.jdr@gmail.com>
2021-08-06 15:06:58 -0600

    subir cambios

--- figuras.js
+++ figuras.js
@@ -1,1 +1,26 @@
-console.log("hello Word")
+//Codigo del cuadrado 
+const ladoCuadrado = 5;
+
+const perimetroCuadrado = ladoCuadrado*4;
+console.log(`el perimetro del cuadro es ${perimetroCuadrado}`);
+
+const areaCuadrado = (ladoCuadrado*ladoCuadrado);
+
+console.log(areaCuadrado);
+
+//fin del cuadrado
+
+//inicio del triangulo
+const ladoTriangulo = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+const alturaTriangulo = 5.5;
+console.log(`los lados del triangulo miden ${ladoTriangulo} ${ladoTriangulo2} ${baseTriangulo}  `);
+
+const perimetroTriangulo = ladoCuadrado + ladoTriangulo2+ baseTriangulo;
+console.log("el perimetro es de:"+ perimetroCuadrado);
+
+const areaCuadrado = (baseTriangulo * alturaTriangulo)/2;
+console.log("el area del triangulo es de " + areaCuadrado);
+
+//fin del triangulo
