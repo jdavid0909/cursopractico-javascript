040dd26ca13e4a9558cc5922e3acde2b8e9e90f6
Josue Rodriguez <jdavid108.jdr@gmail.com>
2021-08-06 15:43:46 -0600

    este la actualizacion

--- figuras.js
+++ figuras.js
@@ -1,26 +1,58 @@
 //Codigo del cuadrado 
 const ladoCuadrado = 5;
 
-const perimetroCuadrado = ladoCuadrado*4;
-console.log(`el perimetro del cuadro es ${perimetroCuadrado}`);
 
-const areaCuadrado = (ladoCuadrado*ladoCuadrado);
+function perimetroCuadrado(lado){
+  return  lado*4;
+} 
+// console.log(`el perimetro del cuadro es ${perimetroCuadrado}`);
 
-console.log(areaCuadrado);
+ function areaCuadrado(ladoCuadrado){
+    return (ladoCuadrado*ladoCuadrado);
+ } 
+
+// console.log(areaCuadrado);
 
 //fin del cuadrado
 
 //inicio del triangulo
-const ladoTriangulo = 6;
-const ladoTriangulo2 = 6;
-const baseTriangulo = 4;
-const alturaTriangulo = 5.5;
-console.log(`los lados del triangulo miden ${ladoTriangulo} ${ladoTriangulo2} ${baseTriangulo}  `);
+// console.group("TRiangulos")
+// const ladoTriangulo = 6;
+// const ladoTriangulo2 = 6;
+// const baseTriangulo = 4;
+// const alturaTriangulo = 5.5;
+// console.log(`los lados del triangulo miden ${ladoTriangulo} ${ladoTriangulo2} ${baseTriangulo}  `);
 
-const perimetroTriangulo = ladoCuadrado + ladoTriangulo2+ baseTriangulo;
-console.log("el perimetro es de:"+ perimetroCuadrado);
 
-const areaCuadrado = (baseTriangulo * alturaTriangulo)/2;
-console.log("el area del triangulo es de " + areaCuadrado);
 
+function perimetroTriangulo(ladoTriangulo,ladoTriangulo2,baseTriangulo){
+    return ladoTriangulo + ladoTriangulo2+ baseTriangulo;
+    }
+// console.log("el perimetro es de:"+ perimetroCuadrado);
+
+function areaTriangulo (baseTriangulo,alturaTriangulo){ 
+    return (baseTriangulo * alturaTriangulo)/2;}
+// console.log("el area del triangulo es de " + areaTriangulo);
+// console.groupEnd();
 //fin del triangulo
+
+//Inicio circulos
+// console.group("Circulo")
+// const radio =4;
+// console.log("radio es " + radio);
+
+function radio(radio){  return radio *2};
+
+// const pi = Math.PI;
+function perimetro (radio){ 
+    var diametro = radio(radio);
+    return diametro* pi;
+}
+// console.log("el perimetro es" + perimetro);
+function areaCirculo(radio) { 
+    
+    return (radio*radio)* Math.PI;
+}
+// console.log("el area es "+ areaCirculo);
+
+// console.groupEnd();
